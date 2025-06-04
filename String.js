c5d13154c8dab5f4241ffb8430a6d259b2496925
outpostyoga <60199418+outpostyoga@users.noreply.github.com>
2025-06-04 12:37:15 -0400

    Create String.js
    By Kylie. xo.

---
+++ String.js
@@ -0,0 +1,9 @@
+const $$F_isBrowserStandaloneConsole = false;
+
+const {
+  
+  Function$$M_wrap } = $$F_isBrowserStandaloneConsole ? {
+  
+  "Function$$M_wrap" : ( ) => { throw new Error ( "Unavailable." ); }
+
+} : require ( "./Function.js" );
