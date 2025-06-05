7d3abf02f55cb77c87ef03ff961aa743fa68c987
outpostyoga <60199418+outpostyoga@users.noreply.github.com>
2025-06-04 21:29:13 -0400

    Update String.js
    It's kinda like scifi. By Kylie. xo.

--- String.js
+++ String.js
@@ -7,3 +7,15 @@ const {
   "Function$$M_wrap" : ( ) => { throw new Error ( "Unavailable." ); }
 
 } : require ( "./Function.js" );
+
+const {
+
+  Object$$O_memcache,
+  
+  Object$$O_settings } = $$F_isBrowserStandaloneConsole ? {
+
+  "Object$$O_memcache" : { },
+  
+  "Object$$O_settings" : { }
+
+} : require ( "./Object.js" );
