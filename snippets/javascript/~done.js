af62b8aca8f94136724f26fdfc0a468865ac5592
outpostyoga <60199418+outpostyoga@users.noreply.github.com>
2025-06-16 08:06:01 -0400

    Delete snippets directory

--- snippets/javascript/~done.js
+++
@@ -1,5 +0,0 @@
-// Done. Log everything and get out of here.
-
-if ( cx.debug ) cx.log.oneliner ( String.chat.i18n ( cx, "SnippetFileName.js" ), String.chat.i18n ( cx, "snippetFunctionName()" ), String.chat.i18n ( cx, "return" ), JSON.stringify ( $0A_return ) );
-
-return $0A_return;
